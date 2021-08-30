1f7d0059ad666c880c1abedfd2e3a3fdde7985fd
adizicher <adikadoche@gmail.com>
2021-08-30 14:54:44 +0300

    writing the IO nn

--- consts.py
+++ consts.py
@@ -2,4 +2,4 @@ SPEAKER_START = 49518  # 'Ġ#####'
 SPEAKER_END = 22560  # 'Ġ###'
 NULL_ID_FOR_COREF = 0
 
-OUT_KEYS = ['pred_logits', 'pred_clusters', 'pred_is_bkgd', 'aux_outputs']
+OUT_KEYS = ['pred_logits', 'pred_clusters', 'pred_is_cluster', 'aux_outputs']
