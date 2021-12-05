ce3f7797a011512c1275ea059922bbd74840312f
adizicher <adikadoche@gmail.com>
2021-12-05 09:22:19 +0200

    adding structural

--- toy_data.py
+++ toy_data.py
@@ -1,10 +1,13 @@
+from collections import namedtuple
 import random
+import math
 import string
 
 LETTERS = string.ascii_lowercase
 LETTERS_LIST = list(LETTERS)
 
-def create_letters_dataser(num_of_texts = 3000):
+def create_letters_dataset(num_of_texts = 3000):
+    #להוסיף סתם טקסט
     text = ''
     clusters = []
     for _ in range(num_of_texts):
@@ -28,7 +31,8 @@ def create_letters_dataser(num_of_texts = 3000):
 
     return text, clusters
 
-def create_sequences_dataser(num_of_texts = 3000):
+def create_sequences_dataset(num_of_texts = 3000):
+    #להוסיף סתם טקסט
     SEQUENCES = []
     for _ in range(70):
         seq_len = random.randint(1, 7)
@@ -54,10 +58,83 @@ def create_sequences_dataser(num_of_texts = 3000):
                     sequence_cluster.append([j, j+len(s)-1])
             if len(sequence_cluster) > 0:
                 clusters[t].append(sequence_cluster)
-            
 
         text += ' '.join(line_list) +'\n'
 
     return text, clusters
 
-create_sequences_dataser(20)
+def create_structural_dataset(num_of_texts = 3000):
+    #להחליט האם החוק הוא רק מימין, רק משמאל, גם וגם.
+    #להחליט מה הרווח מימין ומשמאל
+    #להחליט כמה אותיות הרצפים מימין/משמאל
+    #להחליט מה האותיות של הרצפים מימין/משמאל
+    OneSideSequencePattern = namedtuple("OneSideSequencePattern", ["index_delta", "sequence"])
+    SequencePattern = namedtuple("SequencePattern", ["left", "right", "total_len"])
+    SEQUENCES = []
+    for _ in range(70):
+        left_right_both = random.randint(0, 2)
+        if left_right_both == 0:
+            sequence_pattern_list = [True, True]
+        elif left_right_both == 1:
+            sequence_pattern_list = [True, False]
+        else:
+            sequence_pattern_list = [False, True]
+        
+        total_len = 0
+        for i in range(2):
+            if not sequence_pattern_list[i]:
+                sequence_pattern_list[i] = OneSideSequencePattern(index_delta=0, sequence='')
+            else:
+                sequence_pattern_list[i] = OneSideSequencePattern(index_delta=random.randint(1, 4), \
+                                                                sequence=random.choices(LETTERS, k=random.randint(1, 3)))
+                total_len += sequence_pattern_list[i].index_delta + len(sequence_pattern_list[i].sequence)
+        cur_seq_pattern = SequencePattern(left=sequence_pattern_list[0], right=sequence_pattern_list[1], total_len=total_len+1)
+        if cur_seq_pattern not in SEQUENCES:
+            SEQUENCES.append(cur_seq_pattern)
+
+    text = ''
+    clusters = []
+    for t in range(num_of_texts):
+        clusters.append([])
+        text_len = random.randint(40, 4000)
+        print(text_len)
+        bkgd_text_len = int(random.uniform(0.5, 1) * text_len)
+        sequence_text_len = text_len - bkgd_text_len
+        num_clusters = random.randint(1, min(int(text_len/4), len(SEQUENCES)))
+        cur_sequenceds = random.sample(SEQUENCES, num_clusters)
+        sequence_distribution = [random.uniform(0, 1) for _ in range(len(cur_sequenceds))]
+        sequence_text_len = int(sequence_text_len / (sum([cur_sequenceds[i].total_len * sequence_distribution[i] for i in range(len(cur_sequenceds))]) / sum(sequence_distribution)))
+        line_sequence_list = random.choices(cur_sequenceds, weights=sequence_distribution, k=sequence_text_len)
+        sequence_indices = random.sample(list(range(bkgd_text_len)), sequence_text_len)
+        bgkd_distribution = [random.uniform(0, 1) for _ in range(len(LETTERS))]
+        line_bkgd_list = random.choices(LETTERS, weights=bgkd_distribution, k=bkgd_text_len)
+
+        sorted_pairs = [(x, y) for x, y in sorted(zip(line_sequence_list, sequence_indices), key=lambda pair: pair[1])]
+
+        line_list = line_bkgd_list
+
+        for sequence, index in reversed(sorted_pairs):
+            if index+sequence.right.index_delta >= len(line_bkgd_list) or index-sequence.left.index_delta < 0:
+                continue
+            part_of_text = line_bkgd_list[index-sequence.left.index_delta:index+sequence.right.index_delta+1]
+            cur_sequence = []
+            cur_sequence += sequence.left.sequence if sequence.left.sequence != '' else []
+            cur_sequence += part_of_text
+            cur_sequence += sequence.right.sequence if sequence.right.sequence != '' else []
+            line_list[index:index] = cur_sequence
+        print(len(line_list))
+
+        text += ' '.join(line_list) + '\n'
+        for s in cur_sequenceds:
+            s_cluster = []
+            for i in range(len(line_list) - s.total_len):
+                if line_list[i:i+len(s.left.sequence)] == s.left.sequence and \
+                    line_list[i+len(s.left.sequence)+s.left.index_delta+1+s.right.index_delta:i+s.total_len] == s.right.sequence:
+                    s_cluster.append([i+len(s.left.sequence)+s.left.index_delta, i+len(s.left.sequence)+s.left.index_delta])
+            if len(s_cluster) > 0:
+                clusters[-1].append(s_cluster)
+    return text, clusters
+        
+
+
+create_structural_dataset(20)
