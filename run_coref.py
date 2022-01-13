af9b07c2a65aa7ec7010b73418b9006b2d876e5d
adi <adikadoche@gmail.com>
2022-01-13 10:02:54 +0200

    edited run coref command

--- run_coref.py
+++ run_coref.py
@@ -1,13 +1,13 @@
 import os
 from itertools import combinations
 
-os.system(f"bash run_mem_with_slurm.sh 2 best_block train_slurm.sh --output_dir /home/gamir/adiz/Code/runs/firsttry/output_dir/ --cache_dir /home/gamir/adiz/Code/runs/firsttry/cache_dir/ \
+os.system(f"bash run_mem_with_slurm.sh 2 new_loss_block train_slurm.sh --output_dir /home/gamir/adiz/Code/runs/firsttry/output_dir/ --cache_dir /home/gamir/adiz/Code/runs/firsttry/cache_dir/ \
     --max_eval_print 25 --model_type longformer --model_name_or_path allenai/longformer-large-4096 --tokenizer_name allenai/longformer-large-4096 \
         --config_name allenai/longformer-large-4096 --train_file /home/gamir/datasets/e2e-coref/train.english.jsonlines \
             --predict_file /home/gamir/datasets/e2e-coref/dev.english.jsonlines --do_train --eval all --num_train_epochs 40 --logging_steps 50 --save_steps -1 --eval_steps -1 \
                 --eval_epochs 1 --max_seq_length 4096 --gradient_accumulation_steps 1 --max_total_seq_len 5000 --warmup_steps 5000 --weight_decay 0.01 \
-                    --per_gpu_eval_batch_size 2 --per_gpu_train_batch_size 2 --save_epochs 1 --num_queries 100\
-                        --cluster_block --lr 0.00008 --lr_backbone 0.000008")
+                    --per_gpu_eval_batch_size 1 --per_gpu_train_batch_size 1 --save_epochs 1 --num_queries 100\
+                        --lr 0.00008 --lr_backbone 0.000008 --use_gold_mentions --cluster_block")
 
 # # params = [(("--slots", True), ["slots", "DETR"]),
 # # (("--cluster_block", True), ["block"]),
