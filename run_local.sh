363bf84cac3e1ecfd4dac10c73ba575f44cf88ec
adizicher <adikadoche@gmail.com>
2022-02-05 17:35:30 +0200

    I managed to run on n-401!!

--- run_local.sh
+++ run_local.sh
@@ -25,6 +25,6 @@ PYTHONUNBUFFERED=1 nohup /home/gamir/adiz/miniconda3/envs/torchGPU/bin/python -u
   --train_file /home/gamir/datasets/e2e-coref/train.english.jsonlines --predict_file /home/gamir/datasets/e2e-coref/dev.english.jsonlines --do_train --eval all \
   --num_train_epochs 40 --logging_steps 50 --save_steps -1 --eval_steps -1 --eval_epochs 1 --max_seq_length 4096 --gradient_accumulation_steps 1 \
   --max_total_seq_len 5000 --warmup_steps 5000 --weight_decay 0.01 --per_gpu_eval_batch_size 1 --per_gpu_train_batch_size 1 --save_epochs 1 --num_queries 100 \
-  --slots --use_topk_mentions --topk_pre --max_grad_norm 1.0 --cluster_block --num_junk_queries 150 \
-  --random_queries |  tee ${LOG_PATH}  &
+  --slots --use_topk_mentions --topk_pre --max_grad_norm 1.0 --cluster_block --num_junk_queries 200 \
+  --loss max --topk_lambda 0.7 |  tee ${LOG_PATH}  &
 
